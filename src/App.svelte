7efe40f9c6a6055bc668cce023ccc3ed9f5a16ab
faebebin <fabianbndr@gmail.com>
2023-05-19 19:00:11 +0200

    cleanup

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,5 @@
 <script lang="ts">
-  import svelteLogo from "./assets/svelte.svg";
-  import viteLogo from "/vite.svg";
-  import Counter from "./lib/Counter.svelte";
+  import bouleLogo from "/boule.svg";
   import Prepare from "./lib/Prepare.svelte";
   // TODO turnier name etc.
 </script>
@@ -9,28 +7,14 @@
 <main>
   <div>
     <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
+      <img src={bouleLogo} class="logo" alt="Vite Logo" />
     </a>
   </div>
-  <h1>Vite + Svelte</h1>
+  <h1>Turnier</h1>
 
   <div class="card">
-    <Counter />
     <Prepare />
   </div>
-
-  <p>
-    Check out <a
-      href="https://github.com/sveltejs/kit#readme"
-      target="_blank"
-      rel="noreferrer">SvelteKit</a
-    >, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">Click on the Vite and Svelte logos to learn more</p>
 </main>
 
 <style>
