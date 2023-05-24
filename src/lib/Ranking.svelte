6422050be51360ade32eea3cfb9c22e249106b3a
faebebin <fabianbndr@gmail.com>
2023-05-24 21:59:28 +0200

    Add table-svelte

--- src/lib/Ranking.svelte
+++ src/lib/Ranking.svelte
@@ -1,20 +1,37 @@
 <script lang="ts">
   import { courts, teams } from "../store";
   import type { Court, Team } from "../store";
+  import SvelteTable from "svelte-table";
 
   let teamsList: Team[] = [];
 
   teams.subscribe((tl) => {
     teamsList = tl;
   });
+
+  const columns = [
+    {
+      key: "name",
+      label: "Name",
+      title: "Name",
+      value: (row) => row.name,
+      sortable: true,
+    },
+    {
+      key: "points",
+      label: "Points",
+      title: "Points",
+      value: (row) => row.points,
+      sortable: true,
+    },
+    {
+      key: "members",
+      label: "Members",
+      title: "Members",
+      value: (row) => row.members.map((m) => m.name).join(", "),
+      sortable: true,
+    },
+  ];
 </script>
 
-<ul>
-  {#each teamsList as { name, points, members }, i}
-    <li>
-      <ul>
-        {i + 1}. {name} [{members[0]}, {members[1]}]
-      </ul>
-    </li>
-  {/each}
-</ul>
+<SvelteTable rows={teamsList} {columns} />
