4d99b4f8d0b1d45c3078f8248a68ed0dabd33200
faebebin <fabianbndr@gmail.com>
2023-07-17 20:16:07 +0200

    Use  radio buttons for score

--- src/lib/Court.svelte
+++ src/lib/Court.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
+  import { range } from "lodash";
   import { Card } from "agnostic-svelte";
   import { type Court, type Game, teams } from "../store";
+  import { create_out_transition } from "svelte/internal";
 
   export let court: Court;
   export let game: Game | null = null;
@@ -14,43 +16,106 @@
   <div class="title">{court.name}</div>
 
   {#if game}
-    <label for="home">{getTeamById(game.home).name}</label>
-    <input
-      class="input"
-      id="home"
-      bind:value={game.homeScore}
-      required
-      type="number"
-      min="0"
-      max="13"
-    />
+    <div class="team">{getTeamById(game.home).name}</div>
+    <div class="score">
+      {#each range(0, 7) as score}
+        <label>
+          <input
+            bind:group={game.homeScore}
+            checked={score === 0}
+            type="radio"
+            name="{court.name}-home"
+            value={score}
+          />
+          {score}
+        </label>
+      {/each}
+    </div>
+    <div class="score">
+      {#each range(7, 14) as score}
+        <label>
+          <input
+            bind:group={game.homeScore}
+            type="radio"
+            name="{court.name}-home"
+            value={score}
+          />
+          {score}
+        </label>
+      {/each}
+    </div>
 
-    <label for="visitor">{getTeamById(game.visitor).name}</label>
-    <input
-      class="input"
-      id="visitor"
-      bind:value={game.visitorScore}
-      required
-      type="number"
-      min="0"
-      max="13"
-    />
+    <div class="team">{getTeamById(game.visitor).name}</div>
+    <div class="score">
+      {#each range(0, 7) as score}
+        <label>
+          <input
+            bind:group={game.visitorScore}
+            checked={score === 0}
+            type="radio"
+            name="{court.name}-visitor"
+            value={score}
+          />
+          {score}
+        </label>
+      {/each}
+    </div>
+    <div class="score">
+      {#each range(7, 14) as score}
+        <label>
+          <input
+            bind:group={game.visitorScore}
+            type="radio"
+            name="{court.name}-visitor"
+            value={score}
+          />
+          {score}
+        </label>
+      {/each}
+    </div>
   {/if}
 </Card>
 
 <style>
+  div,
   .title {
     color: white;
+  }
+
+  .team {
+    font-style: italic;
+    text-decoration: underline;
+  }
+
+  .title {
     font-weight: bold;
-    margin-bottom: 1rem;
+    margin-bottom: 0.5rem;
+  }
+
+  .score {
+    width: 100%;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+  }
+
+  label,
+  input {
+    display: block;
+  }
+  input {
+    cursor: pointer;
+    margin: auto;
   }
 
   label {
     color: white;
-  }
-
-  input[type="number"]::-webkit-inner-spin-button,
-  input[type="number"]::-webkit-outer-spin-button {
-    opacity: 1;
+    cursor: pointer;
+    line-height: 0.9em;
+    margin-bottom: 3px;
+    clear: both;
+    /*
+    You can set both the <label> and <input> elements to have display: block, and then add clear: both to the CSS of the <label> element to ensure it clears any float and appears below the <input> element.
+  */
   }
 </style>
